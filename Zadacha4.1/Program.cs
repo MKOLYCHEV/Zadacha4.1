6b2ca92f4204d9dcbb8bb1ac9ff4557fd729d505
Миша Мишин <spawn.rapture-0n@icloud.com>
2021-11-20 18:10:04 +0300

    Добавьте файлы проекта.

---
+++ Zadacha4.1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+            int a = 0;
+            if (num > 0)
+            {
+                for (int i = 1; i <= num; i++)
+                {
+                    a += 2 * i - 1;
+                    Console.WriteLine("Квадрат числа {0} равен {1}", i, a);
+                }
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Ошибка! Введено число меньше единицы.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
